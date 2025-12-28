397cd743cbe00d7d662af6fe35fac88d0520dd4e
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-12-28 11:28:48 +0900

    feat(type): 타입 정의

---
+++ src/types/layout.ts
@@ -0,0 +1,24 @@
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface HeaderProps {
+  isDark: boolean;
+  onThemeToggle: () => void;
+  onLeftSidebarToggle: () => void;
+  onRightPanelToggle: () => void;
+}
+
+export interface LeftSidebarProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export interface RightPanelProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export interface MainContentProps {
+  isRightPanelOpen: boolean;
+}
