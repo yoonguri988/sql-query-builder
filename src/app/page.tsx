c48ab20751f20c1d8bdfc99302d3607fa0a0f3ac
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-12-28 11:35:45 +0900

    fix(upd): Home 화면 구성 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import { Button } from "@/components/ui/button";
+import MainLayout from "@/components/layout/MainLayout";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <h1 className="text-4xl font-bold mb-8">SQL Query Builder</h1>
-        <Button>Test Button</Button>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
-    </div>
-  );
+  return <MainLayout />;
 }
